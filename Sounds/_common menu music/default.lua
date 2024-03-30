821bedaa26514c1ed8e886de9d278bd610282748
barrysir <14061419+barrysir@users.noreply.github.com>
2024-03-30 15:10:39 -0700

    Menu music for April

--- Sounds/_common menu music/default.lua
+++ Sounds/_common menu music/default.lua
@@ -43,11 +43,6 @@ if itIsFriday and gamerTime then
 	file = "FridayNight"
 end
 
--- vocaloid appreciation day
-if MonthOfYear()==2 and (DayOfMonth() >= 9 and DayOfMonth() <= 10) then 
-	file = "Love is war verse"
-end
-
 -- osu day
 if MonthOfYear()==6 and DayOfMonth()==27 then
 	file = "zenith"
@@ -86,6 +81,11 @@ end
 
 if MonthOfYear()==1 then
 	file = "philosophers_polar_north"
+end
+
+-- April, but not April Fool's Day
+if MonthOfYear()==3 and DayOfMonth() >= 2 then
+	file = "love is war"
 end
 
 return THEME:GetPathS("", "_common menu music/" .. file)
